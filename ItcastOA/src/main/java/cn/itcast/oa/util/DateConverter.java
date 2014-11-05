7ec7e7a45e83bead0471925cba0a093cf7044830
lkk <uc12706501@gmail.com>
2014-11-05 21:09:10 +0800

    规范目录结构

--- ItcastOA/src/main/java/cn/itcast/oa/util/DateConverter.java
+++ ItcastOA/src/main/java/cn/itcast/oa/util/DateConverter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cn.itcast.oa;
+package cn.itcast.oa.util;
 
 import com.opensymphony.xwork2.conversion.TypeConversionException;
 import org.apache.struts2.util.StrutsTypeConverter;
