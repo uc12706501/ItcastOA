a7cf0a2c5a6b01f1903842da0aed41134fa758dc
uc12706501 <476362348@qq.com>
2014-11-08 23:11:06 +0800

    1. Hibernate的映射修改为使用注解形式 2. 解决了jta包加载的错误问题

--- ItcastOA/src/main/java/cn/itcast/oa/domain/User.java
+++ ItcastOA/src/main/java/cn/itcast/oa/domain/User.java
@@ -1,20 +1,13 @@
 package cn.itcast.oa.domain;
 
-import javax.persistence.*;
-
 /**
  * Created by lkk on 2014/11/5.
  */
-@Entity
-@Table(name = "itcastoa_user")
 public class User {
     private long id;
 
     private String name;
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public long getId() {
         return id;
     }
@@ -23,7 +16,6 @@ public class User {
         this.id = id;
     }
 
-    @Column
     public String getName() {
         return name;
     }
